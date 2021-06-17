349699e12e5e1acbd8fd3c75ea9f091dd43583fb
KalVasFlame <unnameddwar@gmail.com>
2021-06-17 12:29:32 +0300

    +

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Filter = ({ value, onFilter }) => {
+  return <input type="text" value={value} placeholder="Filter" onChange={onFilter} />
+}
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onFilter: PropTypes.func.isRequired,
+}
+
+export default Filter
